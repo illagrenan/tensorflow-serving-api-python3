9acd8cdaa0441be6052a3c6e4cdb9292f4a656da
Václav Dohnal <vaclav.dohnal@gmail.com>
2018-07-13 23:39:09 +0200

    add Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Operating System :: OS Independent'
     ]
